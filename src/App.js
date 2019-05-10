8b1edd561772392a52e51c4bfb1b7e26f25f4018
Lily <saidumarovanilufar@mail.ru>
2019-05-10 11:27:01 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import Sidebar from './components/Sidebar'
+import Main from './components/Main'
+import api from './components/api'
+import types from './components/type'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    items: [],
+    filter: [],
+  }
+
+  componentDidMount = () => {
+    console.log("api",api)
+    this.setState({ items: api})
+  }
+
+  handleInput = ( e,  itemType ) => {
+    const checked = e.target.checked
+    const newEl = api.filter(el => el.type === itemType)
+    const remove = this.state.filter.filter(el => el.type !== itemType)
+
+    if (checked) {
+      const filter = [...this.state.filter, newEl]
+      this.setState({ filter , items: filter })
+      console.log("filter", filter)
+    } else {
+      alert("del")
+    }
+ 
+    console.log(itemType,  checked)
+    console.log("newEl",newEl)
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <Sidebar
+          types={types}
+          handleInput={this.handleInput}
+        />
+        <Main 
+          items={this.state.items}
+        />
+      </div>
+    );
+  } 
 }
 
-export default App;
+export default App
